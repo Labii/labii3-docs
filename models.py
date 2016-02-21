36d95a19509c355d140d18da26ee8f8bab73f137
Yonggan Wu <weasteam@gmail.com>
2016-02-20 16:51:55 -0800

    added sid

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from common.backend import id2eid
+from common.backend import id2eid, id2eid_static
 
 class Docs(models.Model):
 	titlename = models.CharField(max_length=1000, null=True)
@@ -10,3 +10,6 @@ class Docs(models.Model):
 	
 	def eid(self):
 		return id2eid(self.id)
+	
+	def sid(self):
+		return id2eid_static(self.id)
